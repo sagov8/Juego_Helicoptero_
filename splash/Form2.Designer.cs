ff64e5e5df003a8fa2af528df1bc1fe283cfca2d
valentina2708 <75132700+valentina2708@users.noreply.github.com>
2022-05-09 21:35:32 -0500

    Agregar archivos de proyecto.

---
+++ splash/Form2.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace splash
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_porcentaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(346, 364);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(24, 51);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl_porcentaje
+            // 
+            this.lbl_porcentaje.AutoSize = true;
+            this.lbl_porcentaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_porcentaje.Location = new System.Drawing.Point(639, 413);
+            this.lbl_porcentaje.Name = "lbl_porcentaje";
+            this.lbl_porcentaje.Size = new System.Drawing.Size(37, 24);
+            this.lbl_porcentaje.TabIndex = 1;
+            this.lbl_porcentaje.Text = "0%";
+            this.lbl_porcentaje.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.lbl_porcentaje.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(829, 457);
+            this.Controls.Add(this.lbl_porcentaje);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_porcentaje;
+    }
+}
---
+++ splash/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace splash
+{
+    public partial class Form2 : Form
+    {
+        SoundPlayer cancion = new SoundPlayer(@"D:\UNIVERSIDAD\QUINTO SEMESTRE\Herramientas Visuales\herramientas\splash\assets\dino-2.0.wav");
+
+        int porcentaje = 0;
+        public Form2()
+        {
+            InitializeComponent();
+            cancion.Play();
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (panel1.Width < 515)
+            {
+                panel1.Width += 125;
+                porcentaje += 25;
+                lbl_porcentaje.Text = "" + porcentaje + "%";
+            }
+            else
+            {
+                timer1.Stop();
+                Form3 win2 = new Form3();
+                win2.Show();
+                this.Close();
+               
+            }
+         
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
